ec4c1eff27635687684b2e213748a8bdac17744b
Kostyrev Aleksandr <kostyrev@tutu.ru>
2015-05-11 01:33:30 +0300

    include recipe yum-repoforge only on rhel-family

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,10 @@
 #
 # Copyright (c) 2015 Aleksanr Kostyrev, No Rights Reserved.
 
-include_recipe 'yum-repoforge'
+if platform_family?('rhel')
+  include_recipe 'yum-repoforge'
+end
+
 include_recipe 'packages::base_packages'
 
 
