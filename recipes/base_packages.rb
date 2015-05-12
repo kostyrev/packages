e7163872aa5437e74b1919f376020915fad4e69c
Kostyrev Aleksandr <kostyrev@tutu.ru>
2015-05-12 22:30:52 +0300

    make rubocop happy about recipes/base_packages.rb

--- recipes/base_packages.rb
+++ recipes/base_packages.rb
@@ -4,53 +4,53 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package %w{
-screen
-bind-utils
-telnet
-mc
-tree
-lsof
-yum-utils
-sysstat
-rsync
-xfsprogs
-file
-openssh-clients
-make
-automake
-boost-devel
-rrdtool-perl
-perl-libwww-perl
-htop
-bridge-utils
-mailx
-lynx
-libssh2
-crontabs
-sysfsutils
-sg3_utils
-ncdu
-policycoreutils-python
-yum-plugin-changelog
-iotop
-strace
-zip
-psmisc
-bash-completion
-e2fsprogs
-} do
-    action :install
+package %w(
+  screen
+  bind-utils
+  telnet
+  mc
+  tree
+  lsof
+  yum-utils
+  sysstat
+  rsync
+  xfsprogs
+  file
+  openssh-clients
+  make
+  automake
+  boost-devel
+  rrdtool-perl
+  perl-libwww-perl
+  htop
+  bridge-utils
+  mailx
+  lynx
+  libssh2
+  crontabs
+  sysfsutils
+  sg3_utils
+  ncdu
+  policycoreutils-python
+  yum-plugin-changelog
+  iotop
+  strace
+  zip
+  psmisc
+  bash-completion
+  e2fsprogs
+) do
+  action :install
 end
 
 if platform_family?('rhel') && node['platform_version'].to_i >= 6 && node['platform_version'].to_i < 7
-  package %w{
+  package %w(
     re2c
     ruby-json
     echoping
-  } do
-      action :install
-    end
+  ) do
+    action :install
+  end
 end
 
 package "#{node['man']['package']['name']}" do
@@ -63,23 +63,23 @@ if platform_family?('rhel')
   end
 end
 
-package %w{
+package %w(
   openssl
   bash
-} do
-    action :upgrade
+) do
+  action :upgrade
 end
 
-execute "reload apache2" do
-  command "apachectl graceful"
+execute 'reload apache2' do
+  command 'apachectl graceful'
   action :nothing
-  only_if do ::File.exists?('/usr/sbin/httpd') end
+  only_if { ::File.exist?('/usr/sbin/httpd') }
 end
 
-package %w{
+package %w(
   tzdata-java
   tzdata
-} do
-    action :upgrade
-    notifies :run, 'execute[reload apache2]', :immediately
+) do
+  action :upgrade
+  notifies :run, 'execute[reload apache2]', :immediately
 end
